f02930a5622dcb93b818cfbd6c104be4e3480349
zx <zx@test.com>
2019-10-26 16:45:54 +0800

    link storyboard to the Viewcontroller file

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -14,7 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBAction func weightSlider(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var heightLable: UILabel!
+    
+    @IBOutlet weak var weightLable: UILabel!
+    
+    
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        
+        heightLable.text = "\(String(format: "%2", sender.value))m"
+    }
+    
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightLable.text = "\(String(format: "%0", sender.value))Kg"
+    }
+    
+    
+    @IBAction func caculatePress(_ sender: UIButton) {
+    }
 }
 
